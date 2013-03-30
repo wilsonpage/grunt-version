a42ad62f309a04feab6018d96f79a45abf4a94e7
Wilson Page <wilsonpage@me.com>
2013-03-30 17:29:49 +0000

    First commit

---
+++ tasks/version.js
@@ -0,0 +1,56 @@
+
+/**
+ * Module Dependencies
+ */
+
+var extname = require('path').extname;
+
+/**
+ * Locals
+ */
+
+var regex = {
+	json: /"version":.+"([\d\.]+)"/g,
+	jsdoc: /@version ([\d\.]+)/g,
+	js: /\.version(?:[ \=]+)(?:['"])([\d\.]+)(?:['"])/gi
+};
+
+/**
+ * Exports
+ */
+
+module.exports = function (grunt) {
+
+	grunt.registerMultiTask('version', function(arg) {
+		var version = grunt.version;
+		var set = grunt.option('set');
+
+		if (!set) return console.log(version);
+
+		version = set;
+
+		this.files.forEach(function(file) {
+			file.src.forEach(function(file) {
+				var src = grunt.file.read(file);
+				var ext = extname(file);
+
+				if (ext === '.json') {
+					src = replaceVersion(src, regex.json, version);
+				} else if ('.js') {
+					src = replaceVersion(src, regex.js, version);
+					src = replaceVersion(src, regex.jsdoc, version);
+				}
+
+				grunt.file.write(file, src);
+				grunt.log.writeln('File "' + file + '" version updated to #' + version);
+			});
+		});
+	});
+};
+
+
+function replaceVersion(string, regex, version) {
+	return string.replace(regex, function(match, group) {
+		return match.replace(group, version);
+	});
+}
