8adeaecae90e5e2170973ff209d1529176f5768a
Wilson Page <wilsonpage@me.com>
2013-03-30 17:49:49 +0000

    Convert tabs to spaces

--- tasks/version.js
+++ tasks/version.js
@@ -10,9 +10,9 @@ var extname = require('path').extname;
  */
 
 var regex = {
-	json: /"version":.+"([\d\.]+)"/g,
-	jsdoc: /@version ([\d\.]+)/g,
-	js: /\.version(?:[ \=]+)(?:['"])([\d\.]+)(?:['"])/gi
+  json: /"version":.+"([\d\.]+)"/g,
+  jsdoc: /@version ([\d\.]+)/g,
+  js: /\.version(?:[ \=]+)(?:['"])([\d\.]+)(?:['"])/gi
 };
 
 /**
@@ -21,34 +21,34 @@ var regex = {
 
 module.exports = function (grunt) {
 
-	grunt.registerMultiTask('version', function() {
-		var pkg = grunt.config.data.pkg;
-		var version = grunt.option('set');
+  grunt.registerMultiTask('version', function() {
+    var pkg = grunt.config.data.pkg;
+    var version = grunt.option('set');
 
-		if (!version) return console.log(pkg.version);
+    if (!version) return console.log(pkg.version);
 
-		this.files.forEach(function(file) {
-			file.src.forEach(function(file) {
-				var src = grunt.file.read(file);
-				var ext = extname(file);
+    this.files.forEach(function(file) {
+      file.src.forEach(function(file) {
+        var src = grunt.file.read(file);
+        var ext = extname(file);
 
-				if (ext === '.json') {
-					src = replaceVersion(src, regex.json, version);
-				} else if ('.js') {
-					src = replaceVersion(src, regex.js, version);
-					src = replaceVersion(src, regex.jsdoc, version);
-				}
+        if (ext === '.json') {
+          src = replaceVersion(src, regex.json, version);
+        } else if ('.js') {
+          src = replaceVersion(src, regex.js, version);
+          src = replaceVersion(src, regex.jsdoc, version);
+        }
 
-				grunt.file.write(file, src);
-				grunt.log.writeln('File "' + file + '" version updated to #' + version);
-			});
-		});
-	});
+        grunt.file.write(file, src);
+        grunt.log.writeln('File "' + file + '" version updated to #' + version);
+      });
+    });
+  });
 };
 
 
 function replaceVersion(string, regex, version) {
-	return string.replace(regex, function(match, group) {
-		return match.replace(group, version);
-	});
+  return string.replace(regex, function(match, group) {
+    return match.replace(group, version);
+  });
 }
